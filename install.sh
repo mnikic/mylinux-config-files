729f2d27687f2f8e8c0f433055847d822e426ba6
Loshmi <nikic.milos@gmail.com>
2024-02-09 08:11:50 -0800

    Trimming install.sh

--- install.sh
+++
@@ -1,1 +0,0 @@
-sudo apt install neofetch git perl wireless-tools timeshift i3blocks papirus-icon-theme vim-gtk3 curl build-essential i3 network-manager feh fonts-font-awesome rofi i3blocks htop mlocate && updatedb
