e295343fa84e9941723ab4c23908d463956a2339
Milos Nikic <nikic.milos@google.com>
2024-02-08 17:43:24 -0800

    Adding more tweaks, to volume and other things

---
+++ i3/bin/lock.sh
@@ -0,0 +1,19 @@
+##!/usr/bin/env bash
+
+killall xidlehook
+# Only exported variables can be used within the timer's command.
+export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
+
+# Run xidlehook
+xidlehook \
+  --not-when-fullscreen \
+  --not-when-audio \
+  --timer 300 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+  --timer 180 \
+    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; i3lock -i /home/loshmi/Pictures/lock.png' \
+    '' \
+  --timer 600 \
+    'systemctl suspend' \
+    ''
