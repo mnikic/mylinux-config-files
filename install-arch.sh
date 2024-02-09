e0b69ddc58872bc6e9c305c7c80d2360132b1d00
Loshmi <nikic.milos@gmail.com>
2024-02-09 08:43:23 -0800

    Using --noconfirm for pacman instead of a hacky yes | ...

--- install-arch.sh
+++ install-arch.sh
@@ -3,5 +3,5 @@
 mkdir -p ~/.config ~/Pictures && cp -r i3 ~/.config && cp -r picom ~/.config && cp i3blocks.conf ~/.i3blocks.conf && cp Pictures/* ~/Pictures && cp fehbg ~/.fehbg
 
 #sudo pacman -S --needed - < pkglist.txt
-while read p; do yes | sudo pacman -S --needed $p; done < pkglist.txt
+while read p; do sudo pacman -S --noconfirm --needed $p; done < pkglist.txt
 
