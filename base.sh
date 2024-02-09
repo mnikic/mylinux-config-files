b0948d4aa5ca06907eb12872efb2c7cfbb2531ee
loshmi <nikic.milos@gmail.com>
2024-02-09 15:57:02 -0800

    Change some files permissions, found a better way to change user password in base script, found a better way to reference home in fehbg script and so on

--- base.sh
+++ base.sh
@@ -7,9 +7,8 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo root:password | chpasswd
 
-pacman -S grub efibootmgr networkmanager network-manager-applet xorg-server i3-wm i3blocks i3lock lightdm lightdm-gtk-greeter dialog wpa_supplicant base-devel linux-headers xdg-user-dirs xdg-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call terminus-font xclip
+pacman -S grub efibootmgr networkmanager network-manager-applet xorg xorg-server xorg-xinit i3-wm i3blocks i3lock lightdm lightdm-gtk-greeter dialog wpa_supplicant base-devel linux-headers linux-lts linux-lts-headers xdg-user-dirs xdg-utils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call terminus-font xclip
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
 
@@ -18,12 +17,13 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
+systemctl enable lightdm.service
 
 useradd -m loshmi
-echo loshmi:password | chpasswd
 usermod -aG wheel loshmi
 
 echo "loshmi ALL=(ALL) ALL" >> /etc/sudoers.d/loshmi
-
-
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+RED=$(tput setaf 1)
+printf "${RED}Deed to change loshmi's password in order to be able to log in!!!\e[0m\n"
+echo "$(passwd loshmi)"
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m\n"
--- install-arch.sh
+++ install-arch.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
 mkdir -p ~/.config ~/Pictures && cp -r i3 ~/.config && cp -r picom ~/.config && cp i3blocks.conf ~/.i3blocks.conf && cp Pictures/* ~/Pictures && cp fehbg ~/.fehbg
 
-#sudo pacman -S --needed - < pkglist.txt
 while read p; do sudo pacman -S --noconfirm --needed $p; done < pkglist.txt
-
