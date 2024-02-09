6faedb8c363d1f2b7c831e9e8aee1fa9ec973c53
Milos Nikic <nikic.milos@google.com>
2024-02-08 20:23:00 -0800

    Some small fixes. pacman import doesn't work :(

--- install-arch.sh
+++ install-arch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo pacman -S --needed - < pkglist.txt
 
-mkdir -p ~/.config mkdir -p ~/Pictures && cp -r i3 ~/.config && cp -r picom ~/.config && cp i3blocks.conf ~/.i3blocks.conf && cp Pictures/* ~/Pictures
+mkdir -p ~/.config && mkdir -p ~/Pictures && cp -r i3 ~/.config && cp -r picom ~/.config && cp i3blocks.conf ~/.i3blocks.conf && cp Pictures/* ~/Pictures
